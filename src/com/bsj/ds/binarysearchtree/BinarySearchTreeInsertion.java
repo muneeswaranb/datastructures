ad9d610b358aef5a00e79ae966ad39bdea9a8e33
jisamb5 <muneeswaran.balasubramanianan@jnhunt.com>
2019-12-13 08:21:20 -0600

    Binary Search Tree Insertion

---
+++ src/com/bsj/ds/binarysearchtree/BinarySearchTreeInsertion.java
@@ -0,0 +1,50 @@
+package com.bsj.ds.binarysearchtree;
+
+public class BinarySearchTreeInsertion {
+    Node root=null;
+
+    private Node insert(int value){
+        return null;
+    }
+    private Node insert(Node root,int value){
+        if (root==null){
+            root=new Node(value);
+            //return root;
+        }
+        else if(value<root.data){
+            root.leftNode=insert(root.leftNode,value);
+        }
+        else if(value>root.data){
+            root.rightNode=insert(root.rightNode,value);
+        }
+        return root;
+    }
+    private void preOrder(Node root) {
+        if (root != null) {
+            preOrder(root.leftNode);
+            System.out.println("" + root.data);
+            preOrder(root.rightNode);
+        }
+    }
+
+        public static void main(String args[]){
+            int[] numbers={3,2,6,1,25,4,5,9};
+            Node binarySearchTree=null;
+            BinarySearchTreeInsertion binarySearchTreeInsertion=new BinarySearchTreeInsertion();
+            for (int i = 0; i < numbers.length; i++) {
+                 binarySearchTree=binarySearchTreeInsertion.insert(binarySearchTree,numbers[i]);
+            }
+            binarySearchTreeInsertion.preOrder(binarySearchTree);
+        }
+}
+
+class Node{
+    int data;
+    Node leftNode;
+    Node rightNode;
+
+    public Node(int data){
+        this.data=data;
+        leftNode=rightNode=null;
+    }
+}
