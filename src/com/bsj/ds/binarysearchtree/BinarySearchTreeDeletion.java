eb0f238303c54993542c12932f6633b7e67a04f6
jisamb5 <muneeswaran.balasubramanianan@jnhunt.com>
2020-01-14 08:30:53 -0600

    Binary Search tree deletion correction in case of 2 children.

--- src/com/bsj/ds/binarysearchtree/BinarySearchTreeDeletion.java
+++ src/com/bsj/ds/binarysearchtree/BinarySearchTreeDeletion.java
@@ -8,9 +8,8 @@ public class BinarySearchTreeDeletion {
 
     private Node deleteNode(Node root, int data){
         if(root==null) return root;
-        if(root.data>data)
-            root.leftNode= deleteNode(root.leftNode,data);
-        else if(root.data<data) root.rightNode= deleteNode(root.rightNode,data);
+        if(data<root.data) root.leftNode= deleteNode(root.leftNode,data);
+        else if(data>root.data) root.rightNode= deleteNode(root.rightNode,data);
         else{
             //Case 1: No Child
             //Case 2: Single child
@@ -18,7 +17,7 @@ public class BinarySearchTreeDeletion {
             else if(root.rightNode==null) root=root.leftNode;
             //Case 3: 2 children
             else{
-            int minData=minValue(root);
+            int minData=minValue(root.rightNode);
             root.data=minData;
             root.rightNode = deleteNode(root.rightNode,minData);
         }
@@ -29,8 +28,7 @@ public class BinarySearchTreeDeletion {
     int minValue(Node root)
     {
         int minv = root.data;
-        while (root.leftNode != null)
-        {
+        while (root.leftNode != null){
             minv = root.leftNode.data;
             root = root.leftNode;
         }
@@ -40,11 +38,15 @@ public class BinarySearchTreeDeletion {
     public static void main(String[] args) {
 
         int[] numbers={3,2,6,1,25,4,5,9};
+//                int[] numbers={15,12,13,10,17,23,20,};
         BinarySearchTreeInsertion binarySearchTreeInsertion=new BinarySearchTreeInsertion();
         for (int i = 0; i < numbers.length; i++) {
             root=binarySearchTreeInsertion.insert(root,numbers[i]);
         }
-        root=new BinarySearchTreeDeletion().deleteNode(root,25);
+        System.out.println("Before delete :");
+        binarySearchTreeInsertion.preOrder(root);
+        root=new BinarySearchTreeDeletion().deleteNode(root,3);
+        System.out.println("Afer delete :");
         binarySearchTreeInsertion.preOrder(root);
 //        System.out.println("isDeleted : "+isDeleted);
     }
